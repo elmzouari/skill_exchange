87fc027164ef0329e63d8dd012daec6bd974e248
younes el mezouari <109693920+elmzouari@users.noreply.github.com>
2025-08-17 18:44:52 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,12 @@
+INSTALLED_APPS = [
+    # default django apps...
+    'rest_framework',
+
+    'accounts',
+    'skills',
+    'matches',
+    'messages_app',
+    'reviews',
+    'admin_dashboard',
+]
 
